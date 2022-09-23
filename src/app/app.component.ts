b7d232b79858570768163286c36cc3e1ea329ab5
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2022-09-23 15:16:13 +0200

    responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cv';
+
+  @ViewChild(MatSidenav, { static: true }) sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver){}
+
+  ngOnInit(): void {
+
+    this.observer.observe(['(max-width: 991px)']).subscribe(
+      (state: BreakpointState) => {
+        if (state.matches) {
+            this.sidenav.mode = 'over';
+            this.sidenav.close();
+          console.log('moins de 800px')
+        }else{
+            this.sidenav.mode = 'side';
+            this.sidenav.open();
+          console.log('plus de 800px')
+        }
+      }
+    )
+  }
+
 }
