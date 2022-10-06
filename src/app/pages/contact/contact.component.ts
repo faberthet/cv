5951d74e014af8a67a2306719751852b027f994c
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2022-10-06 18:08:22 +0200

    styles

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  faGithub=faGithub
 
   constructor() { }
 
