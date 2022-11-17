3fe478ffd034d5349e59954b4cb313f6da063099
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2022-11-17 21:56:28 +0100

    styles langages

--- src/app/pages/competences/langages/langages.component.ts
+++ src/app/pages/competences/langages/langages.component.ts
@@ -8,17 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class LangagesComponent implements OnInit {
 
   langages=[ 
-    {id:1, value:"Java",xp:100},
-    {id:2, value:"Javascript",xp:70},
-    {id:3, value:"Typescript",xp:30},
-    {id:4, value:"CSS",xp:100},
-    {id:5, value:"SQL",xp:100}
+    {id:1, value:"Java",xp:'100%'}, //xp en pourcentage de temps sur 2 ans
+    {id:2, value:"Javascript",xp:'70%'},
+    {id:3, value:"Typescript",xp:'30%'},
+    {id:4, value:"CSS",xp:'100%'},
+    {id:5, value:"SQL",xp:'100%'}
 ]
   frameworks=[ 
-    {id:1, value:"Angular",xp:100},
-    {id:2, value:"Spring Boot",xp:70},
-    {id:3, value:"Angular material",xp:30},
-    {id:4, value:"Bootstrap",xp:100},
+    {id:1, value:"Angular",xp:'100%'},
+    {id:2, value:"Spring Boot",xp:'70%'},
+    {id:3, value:"Angular material",xp:'30%'},
+    {id:4, value:"Bootstrap",xp:'100%'},
   ]
 
   constructor() { }
