afd2b7f97805f09580769a3fd6f63f8cdeff3b7f
Fabrice Berthet <berthet-fabrice@hotmail.fr>
2022-11-20 07:51:27 +0100

    styles competences

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -8,6 +9,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 })
 export class ContactComponent implements OnInit {
   faGithub=faGithub
+  faLinkedin= faLinkedin
 
   constructor() { }
 
